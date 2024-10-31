6fc77d471df790aeab5d96f4766d3da13a2799e0
IamSila <silamulingi@gmail.com>
2024-10-31 16:07:24 +0300

    tuple to Tuple from typing modeule

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python3
-
-"""
-Write a type-annotated function to_kv
-that takes a string k and an int OR float v
-as arguments and returns a tuple.
-The first element of the tuple is the string k.
-The second element is the square of the
-int/float v and should be annotated as a float.
-"""
+'''Task 7's module.
+'''
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
-    """takes in a str and a float/int value and returns a float"""
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    '''Converts a key and its value to a tuple of the key and
+    the square of its value.
+    '''
     return (k, float(v**2))
