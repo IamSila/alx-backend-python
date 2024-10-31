8841954ebacd3e3681384a9b4f153b469e2e22ff
IamSila <silamulingi@gmail.com>
2024-10-31 16:05:42 +0300

    tuple to Tuple from typing modeule

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -8,9 +8,9 @@ The first element of the tuple is the string k.
 The second element is the square of the
 int/float v and should be annotated as a float.
 """
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[float, int]) -> tuple[str, float]:
+def to_kv(k: str, v: Union[float, int]) -> Tuple[str, float]:
     """takes in a str and a float/int value and returns a float"""
     return (k, float(v**2))
