b6e6131777f67d9569ec6a17a9d5ad54899ff123
IamSila <silamulingi@gmail.com>
2024-10-31 07:46:06 +0300

    function that concatenates two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""takes in two strings and concatenates them"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    parama: the first string to be extended
+    param b: the second string to be added to str1
+    return: a concatenated string
+    """
+    return str1 + str2
