7639d5bfa0962aae1ef635a9c1229dfb6cd4945d
IamSila <silamulingi@gmail.com>
2024-10-31 07:37:15 +0300

    added docstring

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 
+'''task 0's module to be imported'''
+
+
 def add(a: float, b: float) -> float:
     """
-
     :param a: a float value, which is the first value
     :param b: float value, which is the second argument
     :return: sum, of type float.
