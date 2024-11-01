272769fa1c1e114d02352adcce6a7ccf27c375a0
IamSila <silamulingi@gmail.com>
2024-11-01 11:31:21 +0300

    more advanced and tricky annotations

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""task 10, advanced tasks"""
+from typing import Union, Sequence, Any
+
+
+def safe_first_elements(lst: Sequence[Any]) ->Union[Any, None]:
+    """returns elements from the list if provided, otherwise, none"""
+    if lst:
+        return lst[0]
+    else:
+        return None
