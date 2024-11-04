23cae674bb5b8881df61a3f05a0a05d071393d7f
IamSila <silamulingi@gmail.com>
2024-11-04 09:47:19 +0300

    correction

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -3,10 +3,13 @@
 """
 coroutings and asynchronous processes basic syntax
 """
-import asyncio, random
+import asyncio
+import random
 
-async def wait_random(max_delay: int = 10):
-    """a coroutines that first process the wait time to end then returns the waited time"""
-    t = random.random() * 10
+
+async def wait_random(max_delay: int = 10) -> float:
+    """a coroutines that first process the wait time
+    to end then returns the waited time"""
+    t = random.random() * max_delay
     await asyncio.sleep(t)
     return t
