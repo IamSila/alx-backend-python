88c6080eb7553ac871d36e0829a69bf143630aaf
IamSila <silamulingi@gmail.com>
2024-11-05 19:28:19 +0300

    annoted the function

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -8,7 +8,7 @@ import asyncio
 import random
 from typing import Generator
 
-async def async_generator():
+async def async_generator() -> Generator[int, None, None]:
     """generates a random float number in the given range"""
     for i in range(0, 10):
         number = random.random() * 10
